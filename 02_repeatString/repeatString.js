2404f2a04b8ea1028e9e6de661438a1b8dabb664
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-14 20:16:22 +0200

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, numberOfREpeats) {
+  if(numberOfREpeats < 0) {
+    return "ERROR";
+  }
+  let res = '';
+  for (let i = 0; i < numberOfREpeats; i++) {
+    res += str;
+  }
+  return res;
 };
 
 // Do not edit below this line
