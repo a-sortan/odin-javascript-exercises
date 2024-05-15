ba3aa0d872fd7771aa0bb46e2b9195d52b9c2301
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-15 12:57:04 +0200

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+  return arr.filter(elem => rest.indexOf(elem) === -1)
 };
 
 // Do not edit below this line
