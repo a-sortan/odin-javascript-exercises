48e3d76bd2e6535aaa06751b6dfb4c54dd7cf575
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-15 13:17:41 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+  if(firstNum < 0 || secondNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+    return "ERROR";
+  }
+  let sum = 0;
+  let [min, max] = [firstNum, secondNum];
+  if(firstNum > secondNum) {
+    [min, max] = [secondNum, firstNum];
+  }
+  for(let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
