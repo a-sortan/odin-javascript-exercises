dc2f06fc660a6df9c550599e3ff609e3b55aca59
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-23 10:49:48 +0200

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1;
+  let res = 1;
+  for(let i = num; i > 0; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
