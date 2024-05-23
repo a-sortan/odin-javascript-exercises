41e8d0188393d3524a80454fb6286291940389c8
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-24 00:12:46 +0200

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.replaceAll(/\W/gi, '');
+  newStr = newStr.toLowerCase();
+  let midStr = Math.ceil(newStr.length / 2);
+  for(let i = 0; i < midStr; i++) {
+    let sec = newStr.length - 1 - i;
+    if(newStr[i] !== newStr[sec]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
