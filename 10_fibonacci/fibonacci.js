bf9fff7cd8525289cd75b7c276945285165b2de6
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-25 14:01:22 +0200

    Complete 10_fibbonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let num = +n;
+  if(n == 0 || n == 1) return num;
+  if(n < 0) return 'OOPS';
+  let fibArr = [0, 1];
+  for(let i = 2; i <= num; i++) {
+    let fib = fibArr[i-1] + fibArr[i-2];
+    if(i === num) return fib;
+    fibArr.push(fib);
+  }
 };
 
 // Do not edit below this line
