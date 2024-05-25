bd252a89718cd477fc49a598eb88cba1d916815f
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-25 14:48:35 +0200

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  let yearOfDeath = person.yearOfDeath ?? new Date().getFullYear();
+  return yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(persons) {
+  return persons.reduce((oldest, currentPerson) => {
+    let personAge = getAge(currentPerson)
+    let oldestPersonAge = getAge(oldest);
+    if(personAge > oldestPersonAge) return currentPerson
+    return oldest;
+  });
 };
 
 // Do not edit below this line
